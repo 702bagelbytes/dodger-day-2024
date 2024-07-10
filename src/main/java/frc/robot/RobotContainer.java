3e207840c674c4a30d30d231e6af70bbb20316cd
Dell 3TL2TP2 <702robotics@gmail.com>
2024-07-10 14:33:47 -0700

    drivebase and turret subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,37 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.DriveCommand;
+import frc.robot.commands.TurretCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.TurretSubsystem;
 
 public class RobotContainer {
+  private final DriveSubsystem driveSubsystem;
+  private final TurretSubsystem turretSubsystem;
+  private final CommandXboxController controller;
+
   public RobotContainer() {
+    this.controller = new CommandXboxController(0);
+    this.driveSubsystem = new DriveSubsystem();
+    this.driveSubsystem
+        .setDefaultCommand(new DriveCommand(this.driveSubsystem, controller::getLeftY, controller::getRightX));
+    this.turretSubsystem = new TurretSubsystem();
+    // this.turretSubsystem.setDefaultCommand(new
+    // TurretCommand(this.turretSubsystem, ));
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    this.controller.povRight()
+        .whileTrue(new TurretCommand(turretSubsystem, -0.2))
+        .onFalse(new TurretCommand(turretSubsystem, 0));
+    this.controller.povLeft()
+        .whileTrue(new TurretCommand(turretSubsystem, 0.2))
+        .onFalse(new TurretCommand(turretSubsystem, 0));
+
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
